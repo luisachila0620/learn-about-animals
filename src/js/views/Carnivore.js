04e615768f59f4094637f2b4f1ff5fd79916451e
luisachila0620 <luisarchila038@gmail.com>
2021-10-28 02:00:29 +0000

    added more

--- src/js/views/Carnivore.js
+++ src/js/views/Carnivore.js
@@ -18,7 +18,7 @@ export const Carnivore = () => (
 				<div className="flip-card-inner">
 					<div className="flip-card-front">
 						<img
-							src="https://static.theprint.in/wp-content/uploads/2021/04/yawning-lion.jpg"
+							src="http://all4desktop.com/data_images/original/4242124-lion.jpg"
 							alt="Avatar"
 							className="card-img-top"
 						/>
@@ -36,22 +36,44 @@ export const Carnivore = () => (
 							although some lions scavenge when opportunities occur and have been known to hunt humans,
 							the species typically does not.
 						</p>
-						<a href="https://en.wikipedia.org/wiki/Lion">Visit W3Schools.com!</a>
+						<a className="aa" href="https://en.wikipedia.org/wiki/Lion">
+							to learn more click here!
+						</a>
 					</div>
 				</div>
 			</div>
 		</div>
 
 		<div className="card">
-			<img
-				src="https://media.khou.com/assets/CCT/images/dc946f14-9bbd-4a44-8e72-643069adaab4/dc946f14-9bbd-4a44-8e72-643069adaab4_1140x641.jpg"
-				className="card-img-top"
-				alt="..."
-			/>
-			<div className="card-body">
-				<h5 className="card-titles">Tiger</h5>
+			<div className="flip-card">
+				<div className="flip-card-inner">
+					<div className="flip-card-front">
+						<img
+							src="https://uhdwallpapers.org/uploads/converted/18/06/18/beautiful-big-tiger-1600x900_79896-mm-90.jpg"
+							alt="Avatar"
+							className="card-img-top"
+						/>
+						<h5 className="card-titles">Tiger</h5>
+					</div>
+					<div className="flip-card-back">
+						<h1>Tiger</h1>
+						<p>
+							The tiger (Panthera tigris) is the largest living cat species and a member of the genus
+							Panthera. It is most recognisable for its dark vertical stripes on orange fur with a white
+							underside. It is an apex predator, primarily preying on ungulates such as deer and wild
+							boar. It is territorial and generally a solitary but social predator, requiring large
+							contiguous areas of habitat, which support its requirements for prey and rearing of its
+							offspring. Tiger cubs stay with their mother for about two years, then become independent
+							and leave their mothers home range to establish their own.
+						</p>
+						<a className="aa" href="https://en.wikipedia.org/wiki/Lion">
+							to learn more click here!
+						</a>
+					</div>
+				</div>
 			</div>
 		</div>
+
 		<div className="card">
 			<img
 				src="https://www.nps.gov/articles/images/Image-w-cred-cap_-1200w-_-Brown-Bear-page_-brown-bear-in-fog_2_1.jpg?maxwidth=1200maxheight=1200autorotate=false"
@@ -64,25 +86,69 @@ export const Carnivore = () => (
 		</div>
 		<div className="card-group">
 			<div className="card">
-				<img
-					src="https://cdn.vox-cdn.com/thumbor/6UYSIGEYXWuU3vtgwsCbmT2Nztc=/1400x1050/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/22019271/1184377894.jpg"
-					className="card-img-top"
-					alt="..."
-				/>
-				<div className="card-body">
-					<h5 className="card-titles">Wolf</h5>
+				<div className="flip-card">
+					<div className="flip-card-inner">
+						<div className="flip-card-front">
+							<img
+								src="https://wallpaperaccess.com/full/2693434.jpg"
+								alt="Avatar"
+								className="card-img-top"
+							/>
+							<h5 className="card-titles">Wolf</h5>
+						</div>
+						<div className="flip-card-back">
+							<h1>Wolf</h1>
+							<p>
+								The wolf (Canis lupus[a]), also known as the gray wolf or grey wolf, is a large canine
+								native to Eurasia and North America. More than thirty subspecies of Canis lupus have
+								been recognized, and gray wolves, as colloquially understood, comprise
+								non-domestic/feral subspecies. The wolf is the largest extant member of the family
+								Canidae It is also distinguished from other Canis species by its less pointed ears and
+								muzzle, as well as a shorter torso and a longer tail. The wolf is nonetheless related
+								closely enough to smaller Canis species, such as the coyote and the golden jackal, to
+								produce fertile hybrids with them. The banded fur of a wolf is usually mottled white,
+								brown, gray, and black, although subspecies in the arctic region may be nearly all
+								white.
+							</p>
+							<a className="aa" href="https://en.wikipedia.org/wiki/Wolf">
+								to learn more click here!
+							</a>
+						</div>
+					</div>
 				</div>
 			</div>
+
 			<div className="card">
-				<img
-					src="https://upload.wikimedia.org/wikipedia/commons/0/0a/Standing_jaguar.jpg"
-					className="card-img-top"
-					alt="..."
-				/>
-				<div className="card-body">
-					<h5 className="card-titles">Jarguar</h5>
+				<div className="flip-card">
+					<div className="flip-card-inner">
+						<div className="flip-card-front">
+							<img
+								src="https://free4kwallpapers.com/uploads/wallpaper/chincha-the-jaguar-wallpaper-1600x900-wallpaper.jpg"
+								alt="Avatar"
+								className="card-img-top"
+							/>
+							<h5 className="card-titles">Jaguar</h5>
+						</div>
+						<div className="flip-card-back">
+							<h1>Jaguar</h1>
+							<p>
+								The jaguar (Panthera onca) is a large felid species and the only living member of the
+								genus Panthera native to the Americas. With a body length of up to 1.85 m (6 ft 1 in)
+								and weight of up to 96 kg (212 lb), it is the largest cat species in the Americas and
+								the third largest in the world. Its distinctively marked coat features pale yellow to
+								tan colored fur covered by spots that transition to rosettes on the sides. The jaguars
+								powerful bite allows it to pierce the carapaces of turtles and tortoises, and to employ
+								an unusual killing method, it bites directly through the skull of mammalian prey between
+								the ears to deliver a fatal blow to the brain.
+							</p>
+							<a className="aa" href="https://en.wikipedia.org/wiki/Jaguar">
+								to learn more click here!
+							</a>
+						</div>
+					</div>
 				</div>
 			</div>
+
 			<div className="card">
 				<img
 					src="https://upload.wikimedia.org/wikipedia/commons/1/16/Fox_-_British_Wildlife_Centre_%2817429406401%29.jpg"
