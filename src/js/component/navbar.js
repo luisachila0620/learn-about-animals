a1d0841ccb999c148a1e5d8cefb2ae088134fbd5
luisachila0620 <luisarchila038@gmail.com>
2021-12-10 23:38:53 +0000

    finally deploy

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,11 +3,12 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-dark bg-dark">
-			<Link to="/Contenido">
-				<button className="nav-button">Return</button>
-			</Link>
+		<nav className="navbar navbar-dark bg-dark d-flex juistify-content-end">
+			<div></div>
 			<div className="ml-auto">
+				<Link to="/Contenido">
+					<button className="nav-button">Return</button>
+				</Link>
 				<Link to="/">
 					<button className="secnav-button">Beginning</button>
 				</Link>
