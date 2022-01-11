60497938156937cb009f37a340c0cb9f305815d3
luisachila0620 <luisarchila038@gmail.com>
2022-01-11 02:42:24 +0000

    added logo

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import trns from "../../img/Logo-1.png";
 
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-dark bg-dark d-flex juistify-content-end">
+			<img src={trns} className="logo"></img>
 			<div></div>
 			<div className="ml-auto">
 				<Link to="/Contenido">
